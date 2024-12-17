7434adf8a3a41b10da341de5f384276b33acc8fe
VKx64 <kyllevryann@proton.me>
2024-12-17 23:16:33 +0800

    Fixed Issue: UI Color Mismatch

--- app/src/main/java/vkx64/android/inventorymanagement/MainActivity.java
+++ app/src/main/java/vkx64/android/inventorymanagement/MainActivity.java
@@ -1,5 +1,6 @@
 package vkx64.android.inventorymanagement;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,6 +8,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import vkx64.android.inventorymanagement.Activities.Settings;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +23,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent intent = new Intent(MainActivity.this, Settings.class);
+        startActivity(intent);
     }
 }
